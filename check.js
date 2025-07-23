75e83fad3c73cf4fb4f1f63f76760446524685a1
JAHANDEV-12 <shoxjaxonalisherov79@gmail.com>
2025-07-23 23:41:17 +0500

    Update check.js

--- check.js
+++ check.js
@@ -1077,4 +1077,11 @@ ${products.join('\n')}
         alert("❌ Yuborishda xatolik yuz berdi!");
       }
     })
-    
+    .catch(err => {
+      console.error(err);
+      alert("❗ Internetga ulanmagan ko‘rinadi!");
+    });
+  } else {
+    alert("❗ Bu sahifa Telegram ichida ochilmagan.");
+  }
+}
